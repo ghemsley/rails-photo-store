5130260a890636392e50a434bb9d92d6c3748a18
ghemsley <ghemsley@protonmail.ch>
2021-03-22 20:08:23 -0500

    Add price_modifier model

---
+++ app/models/price_modifier.rb
@@ -0,0 +1,12 @@
+class PriceModifier < ApplicationRecord
+  belongs_to :dimension
+  belongs_to :product, optional: true
+
+  validates :number, numericality: { greater_than_or_equal_to: 0 }, allow_nil: false
+  validates :unit, presence: true, inclusion: { in: %w[usd Usd USD],
+                                                message: '%{value} is not a valid unit, currently only USD is supported' }, allow_nil: false
+
+  def to_s
+    "$#{number.round(2)} #{unit.upcase}"
+  end
+end
