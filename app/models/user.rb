2c324649d7f66ffd8404169e5f78ccb44efcc68a
ghemsley <ghemsley@protonmail.ch>
2021-03-25 04:14:51 -0500

    Update user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,11 @@
 class User < ApplicationRecord
-  has_secure_password
-  has_one :address
-  has_one :cart
+  has_many :quantities
+  has_many :products, through: :quantities
   has_many :orders
   has_many :discounts
+  has_one :address
+  has_one :cart
+
+  validates :email, uniqueness: true
+  validates :uuid, uniqueness: true
 end
