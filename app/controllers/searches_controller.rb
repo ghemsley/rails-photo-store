0078f74085d96090c66d712fe6acff80413fc2e5
ghemsley <ghemsley@protonmail.ch>
2021-03-25 04:11:57 -0500

    Refactor searches controller

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,13 +1,16 @@
 class SearchesController < ApplicationController
   def search
+    @admin = get_admin_if_signed_in
     if !(params[:query].nil? || params[:query] == '')
       @query = search_params(:query)
-      @products = Product.name_or_description_contains(@query)
+      @products = Product.metadata_contains(@query)
     else
       @query = ''
       @products = []
     end
   end
+
+  private
 
   def search_params(*args)
     params.require(*args)
