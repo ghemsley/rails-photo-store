281df95f33658e99a666b4f278c042c3dbeb2c6d
ghemsley <ghemsley@protonmail.ch>
2021-04-01 03:52:46 -0500

    Update User definition and migrations

--- db/migrate/20210315162315_create_users.rb
+++ db/migrate/20210315162315_create_users.rb
@@ -2,9 +2,8 @@ class CreateUsers < ActiveRecord::Migration[6.1]
   def change
     create_table :users do |t|
       t.string :email, null: false
-      t.string :first_name
-      t.string :last_name
-      t.string :uuid, null: false
+      t.string :uuid
+      t.string :password_digest
 
       t.timestamps
     end
