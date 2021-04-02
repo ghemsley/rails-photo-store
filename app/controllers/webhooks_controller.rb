30ef8a7b6bd1c307278c8101a0fc389f36d1508d
Graham Hemsley <ghemsley@protonmail.ch>
2021-04-02 01:27:45 +0000

    Update webhooks controller with fixes

--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -1,30 +1,30 @@
 class WebhooksController < ApplicationController
-  def foxycart_webhook
-    unless request.headers['Foxy-Store-ID'] == '97720' && request.headers['Foxy-Webhook-Event'] == 'transaction/created'
-      return
-    end
+  skip_before_action :verify_authenticity_token
 
+  def foxycart_webhook
     data = ActiveSupport::JSON.decode(request.body.read)
     uuid = data['_embedded']['fx:customer']['id'].to_s
-    user = User.find_or_create_by(uuid: uuid)
-    return unless user.valid?
-
+    user = User.find_or_initialize_by(uuid: uuid)
     email = data['_embedded']['fx:customer']['email']
     user.email = email
-    return unless user.save
+    user.password_digest = data['_embedded']['fx:customer']['password_hash']
+    return 'error' unless user.save
 
     data['_embedded']['fx:items'].each do |item|
       product = Product.find(item['code'])
       next unless product
 
-      quantity = Quantity.find_or_create_by(user_id: user.id, product_id: product.id)
-      next unless quantity.valid?
-
-      quantity.amount += item['quantity']
-      if quantity.save
-        puts "Saved new quantity #{quantity.id} for user #{user.id} and product #{product.id} with amount #{quantity.amount}"
+      quantity = Quantity.find_or_initialize_by(user_id: user.id, product_id: product.id)
+      if quantity.amount == nil
+        quantity.amount = item['quantity']
       else
-        puts quantity.errors.full_messages
+        quantity.amount += item['quantity']
+      end
+      if quantity.save
+        pp "Saved new quantity #{quantity.id} for user #{user.id} and product #{product.id} with amount #{quantity.amount}"
+        render status: :ok, json: @controller.to_json
+      else
+        pp quantity.errors.full_messages
       end
     end
   end
