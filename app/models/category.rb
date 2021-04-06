1f36ae65cc88e5ee01bb0da9bbe03b45ca42ed5d
Graham Hemsley <ghemsley@protonmail.ch>
2021-04-06 15:17:34 +0000

    Update category model slider props json method

--- app/models/category.rb
+++ app/models/category.rb
@@ -8,7 +8,7 @@ class Category < ApplicationRecord
                                                                                too_long: '%{count} characters is the maximum allowed' }
 
   def slider_props_json
-    array = products.collect do |product|
+    products.collect do |product|
       {
         id: product.id,
         name: product.name,
@@ -16,14 +16,12 @@ class Category < ApplicationRecord
         price: product.price,
         price_unit: product.price_unit,
         url: category_product_url(self, product),
-        image: product.image.url,
-        thumbnail: product.image.variant(resize_to_limit: [480, 720]).processed.url,
-        thumbnail_medium: product.image.variant(resize_to_limit: [960, 1440]).processed.url,
-        thumbnail_large: product.image.variant(resize_to_limit: [1440, 2160]).processed.url,
+        image: url_for(product.image),
+        thumbnail: url_for(product.image.variant(resize_to_limit: [480, 720]).processed),
+        thumbnail_large: url_for(product.image.variant(resize_to_limit: [1440, 2160]).processed),
         dimensions_json: product.dimensions_json,
         lightbox: false
       }
-    end
-    JSON.generate(array).to_s
+    end.to_json
   end
 end
